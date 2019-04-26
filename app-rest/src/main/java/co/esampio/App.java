096781bbbf1f8c3f8179c8a54fbc5368c057e361
jhonsanchezgomez <48131958+jhonsanchezgomez@users.noreply.github.com>
2019-04-26 11:40:47 -0500

    Cambio de MYSQL a POSTGRESQL

--- app-rest/src/main/java/co/esampio/App.java
+++ app-rest/src/main/java/co/esampio/App.java
@@ -10,14 +10,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ComponentScan(basePackages = "co.esampio")
 @EnableJpaRepositories(basePackages={"co.esampio.repo.repository"})
+// Se debe extender de SpringBootServletInitializer
 public class App extends SpringBootServletInitializer{
 	
+	//Configuración necesaria para el servidor de aplicacciones WildFly.
 	private static Class<App> applicationClass = App.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
+	//Configuración necesaria para el servidor de aplicacciones WildFly.
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(applicationClass);
