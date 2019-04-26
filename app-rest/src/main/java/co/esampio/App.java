1697ec026948a3c2489182f62147cb6d3e3720c4
Jesus Nicolas Sierra <jnsierrac@gmail.com>
2019-04-26 09:26:06 -0500

    26/04/2019
    Ajustes para la configuracion en Wildfly

--- app-rest/src/main/java/co/esampio/App.java
+++ app-rest/src/main/java/co/esampio/App.java
@@ -2,16 +2,25 @@ package co.esampio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "co.esampio")
 @EnableJpaRepositories(basePackages={"co.esampio.repo.repository"})
-public class App {
+public class App extends SpringBootServletInitializer{
+	
+	private static Class<App> applicationClass = App.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 
 }
